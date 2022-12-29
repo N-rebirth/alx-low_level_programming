dd18cda7d09db2e9f08567dec6ddf3e591207521
N-rebirth <freetrialsix619@gmail.com>
2022-12-29 06:03:53 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  * print_last_digit - prints the last digit
+ *   * @n: the number
+ *    * Return: value of the last digit
+ *
+ */
+int print_last_digit(int n)
+{
+		int x = n % 10;
+		_putchar(x);
+
+		return (0);
+}
