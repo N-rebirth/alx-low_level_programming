9abb54f8bbe1a2c186ade7f31d42f95baaff7810
N-rebirth <freetrialsix619@gmail.com>
2022-12-29 06:26:30 -0800

    int print_last_digit(int n) { int x = n % 20;
    if (x < 0)
    x *= -1;
    
    _putchar(x + 0);
    
    return (0);
    }

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,15 +7,13 @@
  */
 
 int print_last_digit(int n)
-
 {
+	int x = n % 20;
 
-int x = n % 20;
+	if (x < 0)
+		x *= -1;
 
-if (x < 0)
-	x *= -1;
+	_putchar(x + '0');
 
-_putchar(x + '0');
-
-return (0);
+	return (0);
 }
