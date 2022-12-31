89ecc41516045b2e703c2fda53f0d58a85fbbd05
N-rebirth <freetrialsix619@gmail.com>
2022-12-31 09:10:14 -0800

    fizz-buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  * main - print fizzbuzz
+ *   * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("1");
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("Fizz Buzz/n");
+		else if (i % 3 == 0)
+			printf("Fizz\n");
+		else if (i % 5 == 0)
+			printf("Buzz\n");
+		else
+			printf("%d\n", i);
+	}
+	printf("\n");
+	return (0);
+}
