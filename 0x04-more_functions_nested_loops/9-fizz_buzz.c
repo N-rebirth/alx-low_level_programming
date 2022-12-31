861c937d15bbf0f72f1915a9c10305b6d61ad922
N-rebirth <freetrialsix619@gmail.com>
2022-12-31 09:19:54 -0800

    fizz-buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,26 +1,51 @@
 #include "main.h"
+
 #include <stdio.h>
 
 /**
- *  * main - print fizzbuzz
- *   * Return: Always 0
+ *  *  main -   prints the numbers from 1 to 100, followed by a new line
+ *   *  but for multiples of three prints Fizz instead of the number
+ *    *  and for the multiples of five prints Buzz
+ *     *  Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
 	int i;
 
-	printf("1");
 	for (i = 1; i <= 100; i++)
+
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("Fizz Buzz/n");
-		else if (i % 3 == 0)
-			printf("Fizz\n");
-		else if (i % 5 == 0)
-			printf("Buzz\n");
-		else
-			printf("%d\n", i);
+		if (i % 3 == 0 && i % 5 != 0)
+
+		{
+			printf(" Fizz");
+
+		} else if (i % 5 == 0 && i % 3 != 0)
+
+		{
+
+			printf(" Buzz");
+
+		} else if (i % 3 == 0 && i % 5 == 0)
+
+		{
+			printf(" FizzBuzz");
+
+		} else if (i == 1)
+
+		{
+			printf("%d", i);
+		} else
+
+		{
+			printf(" %d", i);
+		}
+
 	}
+
 	printf("\n");
+
 	return (0);
 }
