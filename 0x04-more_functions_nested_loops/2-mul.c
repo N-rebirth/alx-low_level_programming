83bbeed6ba27aa33e138dcb79aa2cfd09317241e
N-rebirth <freetrialsix619@gmail.com>
2022-12-31 05:49:27 -0800

    2-mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *  * mul - multiply 2 integers
+ *   *@a: firdt integer
+ *    *@b: second integer
+ *     * Return: multiplication of integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
